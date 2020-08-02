613edad70a4d316d44bc46cfce743c67b556f8f5
Roger Lamothe <Roger.Lamothe.Jr@gmail.com>
2020-08-02 16:57:51 -0400

    update homework

--- routes/router.js
+++ routes/router.js
@@ -1,13 +1,13 @@
 const router = require("express").Router();
 const exercise = require("../models/exercise.js");
-
+const path = require("path");
 
 router.get("/exercise", function(req, res) {
-    res.sendFile(path.join(_dirname, "../public/exercise.html"));
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
 router.get("stats.js", function(req, res) {
-    res.sendFile(path.join(_dirname, "../public/stats.html"));
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
 
